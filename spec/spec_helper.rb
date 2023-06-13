618779f7e6787544712128dbe88735de0c20da41
Ethan Black <ethanrossblack@gmail.com>
2023-06-13 09:24:09 -0600

    I1/feat: visitor class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'date'
 require 'pry'
 require './lib/visitor'
-require './lib/ride'
-require './lib/carnival'
+#require './lib/ride'
+#require './lib/carnival'
 
 RSpec.configure do |config|
   config.formatter = :documentation
